7efc67303b687f76f760304060449014cd621178
save <csanchez7073@gmail.com>
2022-05-06 08:28:51 -0700

    initial commit

---
+++ assessment-js-basics-1/gitInfo.js
@@ -0,0 +1,84 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+let gitDefinition = "Git is a way to save and track code from a local repository, also used to navigate all files/folders on your machine in a faster fashion. Git can also be used to make working on project with multiple people much easier."
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+let gitHubDefinition = "Github and git are commonly used together but arent the same. Github is where you can find other peoples project code that they have uploaded to get saved, this feature is mostly used for people that you are working with to see changes made to the code that was made. Almost like a code social media."
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+let gitInitDefinition = "Git init initializes a project file or folder, this adds a local repository to your machine and also adds a hidden git file. This step is needed to be able to stage, add, commit, and push later on. "
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+let gitCloneDefinition = "Git clone does as the name suggests, it clones/copies an existing git repository to be added to a new location on your machine or remote machine."
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+let gitStatusDefinition = "Git status displays what files are staged and shows what changes have already been staged. A very good way to check if everything is working correctly in terms of commiting and adding."
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+let gitAddDefinition = "Git add is used to get the working directory ready for a commit, it stages the code to be ready. While Git add on its own doesnt change the code or the directory in any way it is still needed for commiting changes."
+
+let gitAddCode = "git add ."
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+let gitCommitDefinition = "Git commit adds the files that have been staged to your local repository, this is very difficult to undo so it is best to be absolutely sure that what you are commiting is ready and the message reflects what has been commited. "
+let gitCommitCode = 'git commit -m "initial comment"'
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+let gitPushDefinition = "Git push is the command that is used to upload a local repository to a remote repository. As an example this is what you would use to upload all the files that were worked on to github"
---
+++ assessment-js-basics-1/sldc.js
@@ -0,0 +1,35 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Analysis of Requirements
+// Design
+// Implementation
+// Maintain
+// Planning
+// Testing & Integration
+
+// Correct order below
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+let analysisOfRequirements = "This is the second step, this would still be considering a sub-section of the planning stage. This step is where its figured out what is going to be needed for the project, for example if a feature is going to be added to connect with other people or specific features that will be added. "
+let design = "This is the third step, this is where the software is determined on how it will work. Specifies what language will be used, how the users would interact with said application, how it runs relative to servers(communicates with servers)"
+let implementation = "This is the fourth step, all the design that was thought up and figured out in the previous step is now going to be turned into code to make it an actual application. Hence why its called implementation, you are implementing the design as code."
+let testingAndIntegration = "This is the fifth step, this is where all the code that has been implemented is tested to make sure the application runs smoothly and how its supposed to. Its also a way to catch and fix bugs before pushing it out to the public and make changes to better suit what is wanted."
+let maintain = "this is the final step, maintaining a project is a constant step meaning after the code has been deployed to the public the team is able to get feedback from real-world use and see if there are any problems that slipped through the cracks or what can be changed to make user satisfaction better. An application is constantly being updated and this is a primary reason."
